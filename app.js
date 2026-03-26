dfbec0dc30ab77eeacffb9f44920b3bccf0e3602
Mehul Bellukutty <mehulbrainy@gmail.com>
2026-03-26 09:03:40 -0400

    Changes

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ http
         res.end("Hello World!");
     })
     .listen(port);
+
+    const mongoose = require("mongoose");
+
+    //testdb is name of database, it will automatically make it
+    mongoose
+      .connect("mongodb+srv://MehulB:Mehul@gamergauntlet.yehchv0.mongodb.net/")
+      .then(() => console.log("Connected to mongodb..."))
+      .catch((err) => console.error("could not connect ot mongodb...", err));
+    
+    const schema = new mongoose.Schema({
+      name: String,
+    });
+    
+    async function createMessage() {
+      const result = await message.save();
+      console.log(result);
+    }
+    
+    //this creates a Message class in our app
+    const Message = mongoose.model("Message", schema);
+    const message = new Message({
+      name: "Hello World",
+    });
+    
+    createMessage();
